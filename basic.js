0c477df251a07cf4184be7f92d043b2218c70931
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-10-30 19:18:45 +0000

    basicjs

---
+++ basic.js
@@ -0,0 +1,34 @@
+console.log("hello gunjan")
+console.log(2+3)
+console.log(5+6)
+
+// variable can store some information
+// we ca use information later
+// we can change that information later
+
+var firstname = "gunjna"
+var lastname = "kumar"
+console.log(firstname+lastname)
+firstname="kundan"
+console.log(firstname)
+console.log(firstname+lastname)
+
+// let keyword
+// use for declare variable
+let name = 'gunjan kumar'
+console.log(name)
+
+// declare constants
+
+const pi=3.14
+// not allowed  pi=3.15
+let r = 2
+console.log(pi*2*2)
+
+// string indexing
+
+let name2 = "gunjan"
+
+console.log(name[2])
+console.log(name2.length)
+
