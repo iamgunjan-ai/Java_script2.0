36546230d0950fca50d6ef9d0fcbf2065af84b83
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-10-30 19:40:57 +0000

    string

---
+++ basic2.js
@@ -0,0 +1,29 @@
+
+// string indexing
+
+let name2 = "gunjan"
+
+console.log(name2[2])
+console.log(name2.length)
+
+// trim 
+let name ="  gunjan   "
+console.log(name.length)
+name.trim()
+console.log(name.length)
+let newname=name.trim()
+console.log(newname)
+console.log(name.length)
+newname = newname.toUpperCase()
+console.log(newname)
+
+//slicing
+//starting 
+//ending index
+
+
+let newsting=newname.slice(0,4)
+console.log(newsting)
+
+
+
