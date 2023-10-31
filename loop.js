e9b81a3c94556748a9ac0737c9c4f57cb4138bcf
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-10-31 08:38:29 +0000

    loop

---
+++ loop.js
@@ -0,0 +1,28 @@
+// while loop
+let num =0
+while(num<=10){
+    console.log(num)
+    num++
+}
+// sum  n natural number
+
+let number = 10
+let sum=0
+let i=0
+while(i<=number){
+    sum +=i
+    i++
+}
+console.log(sum)
+
+// for loop
+
+let num1=10
+let sum1 =0
+let j=0
+for(let j=0;j<num1;j++){
+    sum1 +=i 
+}
+console.log(sum)
+
+// 
