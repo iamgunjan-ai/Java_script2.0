d9a8a06d359e79ff1f2363cbd74b19eb1bfd31e1
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-10-31 05:04:10 +0000

    datatypes

---
+++ operator.js
@@ -0,0 +1,62 @@
+// data type
+
+// string
+// number
+// booleans
+//undefined
+// null
+// big int
+// symbol
+
+let age = 22
+let firstname = "gunjan"
+console.log(typeof age)
+console.log(typeof firstname)
+
+//convert nummber to string
+
+age = age +"";
+console.log(age)
+console.log(typeof age)
+
+//convert string to number
+
+let mystr = "35"
+console.log(typeof mystr)
+newnum = +mystr
+console.log(newnum)
+console.log(typeof newnum)
+
+let myvar = null
+console.log(myvar)
+console.log(typeof myvar)
+
+ // bug ,error in javascript
+
+ //big int
+ let num=123
+ console.log(num.MAX_SAFE_INTERGER)
+
+ // booleans and comparison operator
+ let num1 =7
+ let num2=7
+ console.log(num1>num2)
+ console.log(num1>=num2)
+ console.log(num1<num2)
+
+ // ==  vs ===  == is use only use to value check and === use to value with data type
+let num3 = "7"
+ console.log(num1==num2);
+ console.log(num1===num3)
+
+ console.log(num1 !=num3)
+
+ //truthy and false values
+
+ // falsy value 
+ //false
+ //""
+ //undefine
+ 
+
+
