4c2ae19c44540b80f54e61a639b67a051f20d6fc
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-11-02 10:47:19 +0000

    Array

---
+++ Array.js
@@ -0,0 +1,35 @@
+//JavaScript Array is a single variable that is used to store elements of different data types.
+
+//  Declaration of array
+//let arrayName = [value1, value2, ...];
+let num = [5,6,3,9,4]
+console.log(num)
+num.push(9)
+console.log(num)
+num.pop()
+console.log(num)
+console.log(num[2])
+console.log(num.length)
+
+//Loop through Javascript Array Elements
+
+let num1 = [5,6,3,9,4]
+
+for(let i=0;i<num1.length;i++){
+    console.log(num1[i])
+}
+console.log(typeof num1)
+
+console.log(Array.isArray(num1))
+
+// Javascript Array.push() Method
+// Javascript Array.unshift() Method
+// JavaScript Array.pop() Method
+// JavaScript Array.shift() Method
+// JavaScript Array.splice() Method
+// JavaScript Array.slice() Method
+// JavaScript Array some() Method
+// JavaScript Array reduce() Method
+// JavaScript Array map() Method
+
+
