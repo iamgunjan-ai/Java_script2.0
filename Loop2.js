f9d3d00230c4dc37e2da518748c94f00c7e9b224
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-12-14 17:35:36 +0000

    Loop2

---
+++ Loop2.js
@@ -0,0 +1,35 @@
+//loop
+// to execute a paice of code again and again
+
+// while, do while,for loop
+
+
+let num=5;
+for(let i=0;i<=num;i++){
+    console.log(i);
+}
+// sum of n number
+let sum=0;
+for(let i=0;i<=num;i++){
+    sum +=i;
+}
+console.log("sum:",sum);
+
+//for in loop
+//for(let key in object)
+
+let n=100;
+for(let i=1;i<=100;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+
+// string
+
+let str="gunjankumar"
+console.log(str.length)
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
