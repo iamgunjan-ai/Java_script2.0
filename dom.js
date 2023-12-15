514386aaca04d47d7b0e336270ec47ba02ac7dd4
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-12-15 15:23:29 +0000

    Dom

---
+++ dom.js
@@ -0,0 +1,58 @@
+console.log(window)
+
+// when  a web page is loaded the browser creates a document object  mode(DOM)
+//of the page
+
+// it is use in dynamic change 
+
+
+console.log(document.body);
+console.log(document.head);
+
+// DOM maninpulation
+
+// selecting with id
+// documnet.getElementById("myid")
+
+// selecting with class
+//document.getElementByClassName("myclass")
+
+// selecting with tag
+
+// document.getElementByTagName("p")
+
+// let heading = documnet.getElementById("heading")
+//console.log(heading)
+
+
+// let heading = documnet.getElementsByClassName("myclass")
+//console.log(heading) 
+
+// attributes 
+// getAttribute(attr)
+// setAttribute(att,value)
+
+// style
+// Node.style
+
+//proprties
+
+// tagName : returns tag for element nodes
+// innerText : returns the text content of the element and all its children
+// innerHTML : returns the plain text or HTML contents in the element
+// textContent : returns textual content even for hidden elements
+
+// node.append( el ) //adds at the end of node (inside)
+// Insert Elements
+// node.prepend( el ) //adds at the start of node (inside)
+// let el = document.createElement(“div“)
+// node.before( el ) //adds before the node (outside)
+// node.after( el ) //adds after the node (outside)
+//node.remove( ) //removes the
+
+
+
+
+
+
+
